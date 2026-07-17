0beb4e44f8e7293e278c19444f6b3a0f0eb1ad9d
Chitransh <dev@Developers-MacBook-Air.local>
2026-07-17 15:15:48 +0530

    feat: add video gallery section above patient resources with smooth lightbox playback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Marquee from "../components/Marquee";
 import Services from "../components/Services";
+import VideoGallery from "../components/VideoGallery";
 import PatientResources from "../components/PatientResources";
 import BeforeAfterSlider from "../components/BeforeAfterSlider";
 import Equipments from "../components/Equipments";
@@ -20,6 +21,7 @@ export default function Home() {
       <Hero />
       <Marquee />
       <Services />
+      <VideoGallery />
       <PatientResources />
       <BeforeAfterSlider />
       <About />
